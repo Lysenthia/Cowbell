c9d532c7673915da4f30a27590c8622c41710f5d
Lysenthia <mishtopher@gmail.com>
2017-05-26 09:07:24 +1200

    Added the tiniest bit extra functionality
    You can now name the file

--- Backend_Experimentation/sound_array_test_with_crossfade.py
+++ Backend_Experimentation/sound_array_test_with_crossfade.py
@@ -5,13 +5,16 @@ from pydub import AudioSegment
 CROSSFADE_LENGTH = 50
 UNIT_LENGTH = 2
 
-def make_wave(notes_to_play='C4C4C5C5B4G4A4B4C5C4C4A4A4G4G4G4G4'):
+def make_wave(notes_to_play='C4C4C5C5B4G4A4B4C5C4C4A4A4G4G4G4G4', outfile_name=None):
     """ Makes the song from notes_to_play"""
     creation_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")
     notes = [notes_to_play[i:i+UNIT_LENGTH] for i in range(0, len(notes_to_play), UNIT_LENGTH)]
     infiles = ['sound_array/{}.wav'.format(x) for x in notes]
     print(infiles)
-    outfile = "music{}.wav".format(creation_date)
+    if name == None:
+        outfile = "music{}.wav".format(creation_date)
+    else:
+        outfile = outfile_name
     parameters = (1, 2, 44100, 0, 'NONE', 'not compressed')
     combinedAudio = AudioSegment.from_wav(infiles[0])
     infiles.pop(0)
