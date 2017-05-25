119ecdc51267e1cddc00464bc69116946c913d43
Lysenthia <mishtopher@gmail.com>
2017-05-26 09:02:22 +1200

    Made output files unique

--- Backend_Experimentation/sound_array_test_with_crossfade.py
+++ Backend_Experimentation/sound_array_test_with_crossfade.py
@@ -1,4 +1,5 @@
 import wave
+import datetime
 from pydub import AudioSegment
 
 CROSSFADE_LENGTH = 50
@@ -6,10 +7,11 @@ UNIT_LENGTH = 2
 
 def make_wave(notes_to_play='C4C4C5C5B4G4A4B4C5C4C4A4A4G4G4G4G4'):
     """ Makes the song from notes_to_play"""
+    creation_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")
     notes = [notes_to_play[i:i+UNIT_LENGTH] for i in range(0, len(notes_to_play), UNIT_LENGTH)]
     infiles = ['sound_array/{}.wav'.format(x) for x in notes]
     print(infiles)
-    outfile = "music2.wav"
+    outfile = "music{}.wav".format(creation_date)
     parameters = (1, 2, 44100, 0, 'NONE', 'not compressed')
     combinedAudio = AudioSegment.from_wav(infiles[0])
     infiles.pop(0)
