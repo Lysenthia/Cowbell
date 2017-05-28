2365f54a68a6d4ea67c5f69c821bb0589284fab5
Henry Nicholson <dr.ex10011@gmail.com>
2017-05-29 11:43:30 +1200

    Commented the hacked together version because hamish is a genuis

--- Frontend/public/routes_hacked_together.py
+++ Frontend/public/routes_hacked_together.py
@@ -34,33 +34,24 @@ def synth(notes = None):
 def exported():
         noteDict = dict(((0, "C4"), (1, "D4"), (2, "E4"), (3, "F4"), (4, "G4"), (5, "A4"), (6, "B4"), (7, "C5")))
         print(noteDict)
+
+        #Values of the sliders once they come out
         slider_values_true = {}
+        #List of inputted notes after they've been converted from the slider values
         note_values = []
+        #get the slider values from the page
         sliderValues = request.values if request.method == "GET" else request.values
-	#response = "Form Contents <pre>%s</pre>" % "<br/>\n".join(["%s:%s" % item for item in formData.items()] )
-	#sliderValues.pop('exporttowav', None)
-        #THERE ARE MAJOR ERRORS HERE FIX IT
-        #print(sliderValues)
-        #for item in sliderValues:
-        #        print(item)
-        #print("Working")
+        #Extract data from the slider values and remove "exporttowav"
         for key in sliderValues:
                 if key != "exporttowav":
                         print(key)
-                        #print(sliderValues[key])
                         slider_values_true[key] = sliderValues[key]
         print(slider_values_true)
-        #[note_values.append(noteDict[item]) for item in list(slider_values_true.values())]
+        #Adding the corresponding slider values to the note values
         for item in list(slider_values_true.values()):
                 note_values.append(noteDict[int(item)])
         print(note_values)
+        #Pretty it up
         string = ''.join(note_values)
-        #print(note_values)
-        #print(note_values)
-        #outvalues = outvalue1.to_dict()
-        #print(outvalues)
-	#for value in sliderValues:
-	#	noteValues.append(noteDict[value])
-	#print(sliderValues)
+        #This will eventually not return as a string
         return string
-	#return render_template('exported.html')
