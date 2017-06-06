8546a1062b99a69cf5a19fd5fbdabeb1bcced041
Lysenthia <mishtopher@gmail.com>
2017-06-06 14:14:55 +1200

    Working on creating a downloadable database

--- Backend_Experimentation/Database_Testing/database_testing.py
+++ Backend_Experimentation/Database_Testing/database_testing.py
@@ -1,7 +1,10 @@
-import sqlite3, itertools
+import sqlite3
 INFILE = "saved_song.cowbell"
-with sqlite3.connect(INFILE) as db:
-    cursor = db.cursor()                                                                                                                        #Sets up cursor
-    cursor.execute("select SongNotes from song_data")
-    song_notes = str(cursor.fetchall())
+try:
+    with sqlite3.connect(INFILE) as db:
+        cursor.execute("select SongNotes from song_data")
+        song_notes = cursor.fetchall()
+        song_notes = "".join(map("".join, song_notes))
     print(song_notes)
+except FileNotFoundError:
+    print("Error 404 - File not found :-p")
