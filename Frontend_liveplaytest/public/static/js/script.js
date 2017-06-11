6b3759821bdf9b03ae7476cd8568709420aa5902
Henry Nicholson <dr.ex10011@gmail.com>
2017-06-12 11:18:18 +1200

    Duplicated the Frontend to experiment with live playing

--- Frontend_liveplaytest/public/static/js/script.js
+++ Frontend_liveplaytest/public/static/js/script.js
@@ -1,4 +1,5 @@
-function change(button_name) {
+function change(button_name) // no ';' here
+{
     var elem = document.getElementsByName(button_name);
     if (elem.innerHTML =="Link") elem.innerHTML = "Unlink";
     else elem.innerHTML = "Link";
