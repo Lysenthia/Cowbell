a73aecd17596d2bff71adbdf81397fa784aaa2d4
Lysenthia <mishtopher@gmail.com>
2017-06-15 14:07:42 +1200

    Preparing to begin work on making databases uploadable

--- Lysenthia_Frontend_Modification/public/routes.py
+++ Lysenthia_Frontend_Modification/public/routes.py
@@ -3,23 +3,26 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask import send_from_directory
+from flask import flash
 from public import website
-from generator import make_wav
+from generator import Song
 import os
+
 
 #HOMEPAGE/NOTE AMOUNT ENTER
 @website.route('/', methods=['GET', 'POST'])
 def index():
-        error = None
-        if request.method == 'POST':
-                noteamt = request.form.get('notes')
-		# THIS MAKES AN ERROR FIX IT UP AT SOME POINT PLZZ
-                if noteamt == "":
-                        error = "Please enter a number."
-                else:
-                        return redirect('/synth/{}'.format(noteamt))
-        else:
-                return render_template('index.html', error=error)
+	error = None
+	if request.method == 'POST':
+		noteamt = request.form.get('notes')
+		if noteamt == "" or noteamt == "0":
+			error = "Please enter a non-zero number."
+			return render_template('index.html', error=error)
+		else:
+			noteamt = abs(int(noteamt))
+			return redirect('/synth/{}'.format(str(noteamt)))
+	else:
+		return render_template('index.html', error=error)
 
 
 
@@ -29,36 +32,43 @@ def synth(notes = None):
 	#if request.method == 'POST':
 	#	print("Sup")
 	#else:
-        return render_template('synth.html', notes=notes)
+	return render_template('synth.html', notes=notes)
 
 
 #DISPLAYS WHEN WAV EXPORTED
 @website.route('/exported', methods=['GET', 'POST'])
 def exported():
-        noteDict = dict(((0, "C4"), (1, "D4"), (2, "E4"), (3, "F4"), (4, "G4"), (5, "A4"), (6, "B4"), (7, "C5")))
-        slider_values_true = {}
-        note_values = []
-        sliderValues = request.values if request.method == "GET" else request.values
-        for key in sliderValues:
-                if key != "exporttowav":
-                        print(key)
-                        slider_values_true[key] = sliderValues[key]
-        sliderKeys = sorted(slider_values_true)
-        for item in sliderKeys:
-                note_values.append(noteDict[int(slider_values_true[item])])
-        string = ''.join(note_values)
-        outfile_name = make_wav(string)
-#        if request.method == 'POST':
-#                return redirect('/return-file/{}'.format(outfile_name))
-#        else:
-#               return render_template('exported.html', outfile_name = outfile_name)
-        return render_template('exported.html', outfile_name = outfile_name)
-    
+	#Dictionary of the slider values that correspond to the note values
+	noteDict = {0:"C4", 1:"D4", 2:"E4", 3:"F4", 4:"G4", 5:"A4", 6:"B4", 7:"C5"}
+	sliderValues = {}
+	noteValues = []
+	sliderKeys = []
+	#Get the submitted slider values from the previous page
+	rawSliderValues = request.values if request.method == "GET" else request.values
 
-@website.route('/return-file/<outfile_name>')
-def return_file(outfile_name):
-    outfile_name = outfile_name
-    directory = os.getcwd()
-    return send_from_directory(directory, outfile_name, 
-                     attachment_filename=outfile_name,
-                     as_attachment=True, mimetype='audio/wav')
+	#Extract the data from rawSliderValues and add it to sliderValues (Also get rid of "exporttowav")
+	for key in rawSliderValues:
+		if key != "exporttowav":
+			temp_key = int(key[6:])
+			sliderValues[temp_key] = rawSliderValues[key]
+
+	#Create a sorted list of the keys from slider values
+	sliderKeys = sorted(sliderValues)
+	print(sliderKeys)
+	###  Get each note value from the noteDict in order using the sorted sliderKeys 
+	###  list to call values from sliderValues in the original order
+	for item in sliderKeys:
+		noteValues.append(noteDict[int(sliderValues[item])])
+
+	#Convert noteValues to string
+	sNoteValues = ''.join(noteValues)
+	#Create WAV file from the string version of noteValues
+	song = Song(sNoteValues)
+	wavFileName = song.make_wav()
+	#Return the exported page and attach the filename of the exported WAV file
+	return render_template('exported.html', wavFileName = wavFileName)
+
+@website.route('/return-file/<wavfilename>')
+def return_file(wavfilename):
+	directory = os.getcwd()
+	return send_from_directory(directory, wavfilename, attachment_filename=wavfilename, as_attachment=True, mimetype='audio/wav')
--- Lysenthia_Frontend_Modification/public/static/js/script.js
+++ Lysenthia_Frontend_Modification/public/static/js/script.js
@@ -1,5 +1,4 @@
-function change(button_name) // no ';' here
-{
+function change(button_name) {
     var elem = document.getElementsByName(button_name);
     if (elem.innerHTML =="Link") elem.innerHTML = "Unlink";
     else elem.innerHTML = "Link";
