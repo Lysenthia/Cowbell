1dc4316425a90de93c7bc8bf4a8d27c5ac4ea86a
Lysenthia <mishtopher@gmail.com>
2017-08-01 14:40:58 +1200

    Added some extra functionality

--- Backend_Experimentation/Server_Side_Database/create_database.py
+++ Backend_Experimentation/Server_Side_Database/create_database.py
@@ -1,13 +1,14 @@
 """
 Created on Fri Jul 28 11:22:34 2017
 @author: Lysenthia
+REMINDER: rework later so that DB_NAME and DB_DIRECTORY are passed to the functions
 """
 
 def create_database():
     """ Creates the initial database """
     import sqlite3, os
     print(os.getcwd())
-    DB_NAME = 'cloud_storage.db'
+    DB_NAME = 'cloud_storage.cowbell_server'
     DB_DIRECTORY = '/server_side_storage'
     db = sqlite3.connect('{}{}'.format(DB_DIRECTORY, DB_NAME))
     cursor = db.cursor()
@@ -37,6 +38,21 @@ def add_project(uid, song_notes, author_name, creation_date, project_name):
     DB_DIRECTORY = '/server_side_storage'
     db = sqlite3.connect('{}{}'.format(DB_DIRECTORY, DB_NAME))
     cursor = db.cursor()
-    cursor.execute("INSERT INTO ? VALUES (NULL, ?,?,?,?)",(uid, song_notes, author_name, creation_date, project_name))
+    cursor.execute("SELECT user_table_name FROM user_ids WHERE uid=?",(uid))
+    table_name = cursor.fetchall()
+    cursor.execute("INSERT INTO ? VALUES (NULL, ?,?,?,?)",(table_name, song_notes, author_name, creation_date, project_name))
     db.commit()
     db.close()
+    
+def get_uids():
+    """ Gets all uids and returns them as a list """
+    import sqlite3
+    DB_NAME = 'cloud_storage.db'
+    DB_DIRECTORY = '/server_side_storage'
+    db = sqlite3.connect('{}{}'.format(DB_DIRECTORY, DB_NAME))
+    cursor = db.cursor()
+    cursor.execute("SELECT uid FROM user_ids")
+    all_uids = cursor.fetchall()
+    db.commit()
+    db.close()
+    return all_uids
