9755b9f3268f917af8be3466c9979adef73594f9
Lysenthia <mishtopher@gmail.com>
2017-08-03 11:45:40 +1200

    Switched to uuid, shouldn't kill computers now

--- Backend_Experimentation/UIDs/generate_uid.py
+++ Backend_Experimentation/UIDs/generate_uid.py
@@ -4,9 +4,7 @@ Created on Thu Aug  3 11:12:49 2017
 
 @author: Lysenthia
 """
-
-import base64
-import random
+import uuid
 test_uids = []
-number = random.choice(list(set(range(10 ** 99, int('9'*100)))-set(test_uids)))
-print(number)
+uid = uuid.uuid4().hex
+print(uid)
