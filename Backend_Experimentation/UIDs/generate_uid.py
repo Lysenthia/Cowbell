1d6a7cdda20e22c128354a396e9fa0264b1b1e5b
Lysenthia <mishtopher@gmail.com>
2017-08-04 09:24:52 +1200

    Did a bit more work on the database

--- Backend_Experimentation/UIDs/generate_uid.py
+++ Backend_Experimentation/UIDs/generate_uid.py
@@ -5,6 +5,8 @@ Created on Thu Aug  3 11:12:49 2017
 @author: Lysenthia
 """
 import uuid
-test_uids = []
+test_uids = ['']
 uid = uuid.uuid4().hex
+while uid in test_uids:
+    uid = uuid.uuid4().hex
 print(uid)
