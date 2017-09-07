9c8912d6502da1ce35c41fc0c5b23e85d1404b96
Henry Nicholson <dr.ex10011@gmail.com>
2017-09-07 14:41:01 +1200

    Oh my god we did so much stuff today it might work with multiple formats soon! (we gave up on json then used a ton of json afterwards)

--- Frontend/public/routes.py
+++ Frontend/public/routes.py
@@ -54,37 +54,39 @@ def synth(notes = None):
 #DISPLAYS WHEN WAV EXPORTED
 @website.route('/exported', methods=['GET', 'POST'])
 def exported():
-	if request.method == 'POST':
-		print(jsondata)
-	else:
-		#Dictionary of the slider values that correspond to the note values
-		noteDict = {0:"C4", 1:"D4", 2:"E4", 3:"F4", 4:"G4", 5:"A4", 6:"B4", 7:"C5"}
-		sliderValues = {}
-		noteValues = []
-		sliderKeys = []
-		#Get the submitted slider values from the previous page
-		rawSliderValues = request.values if request.method == "GET" else request.values
-    	#Extract the data from rawSliderValues and add it to sliderValues (Also get rid of "exporttowav")
-		for key in rawSliderValues:
-			if key != "exporttowav":
-				temp_key = int(key[6:])
-				sliderValues[temp_key] = rawSliderValues[key]
-    
-    	#Create a sorted list of the keys from slider values
-		sliderKeys = sorted(sliderValues)
-		print(sliderKeys)
-    	#  Get each note value from the noteDict in order using the sorted sliderKeys 
-    	#  list to call values from sliderValues in the original order
-		for item in sliderKeys:
-			noteValues.append(noteDict[int(sliderValues[item])])
-    
-    	#Convert noteValues to string
-		sNoteValues = ''.join(noteValues)
-		jsondata = jsonify(songdata=sNoteValues, author_name='Anon', outfile_name=None, cloud_db_pos=None)
-		print("JSON: ".format(jsondata))
-		print(type(jsondata))
-    	#Return the exported page and attach the filename of the exported WAV file
-		return render_template('exported.html', jsondata=jsondata)
+	# if "audioformats" in request.form:
+	# 	audioformat = request.form.get("audioformats")
+	# 	return redirect('downloader/audiofile/{}'.format(audioformat))
+
+	#Dictionary of the slider values that correspond to the note values
+	noteDict = {0:"C4", 1:"D4", 2:"E4", 3:"F4", 4:"G4", 5:"A4", 6:"B4", 7:"C5"}
+	sliderValues = {}
+	noteValues = []
+	sliderKeys = []
+	#Get the submitted slider values from the previous page
+	rawSliderValues = request.values if request.method == "GET" else request.values
+	#Extract the data from rawSliderValues and add it to sliderValues (Also get rid of "exporttowav")
+	for key in rawSliderValues:
+		if key != "exporttowav":
+			print(key)
+			temp_key = int(key[6:])
+			sliderValues[temp_key] = rawSliderValues[key]
+
+	#Create a sorted list of the keys from slider values
+	sliderKeys = sorted(sliderValues)
+	print(sliderKeys)
+	#  Get each note value from the noteDict in order using the sorted sliderKeys 
+	#  list to call values from sliderValues in the original order
+	for item in sliderKeys:
+		noteValues.append(noteDict[int(sliderValues[item])])
+
+	#Convert noteValues to string
+	sNoteValues = ''.join(noteValues)
+	jsondata = json.dumps({"songdata":sNoteValues, "author_name":'Anon', "outfile_name":None, "cloud_db_pos":None})
+	print("JSON: ".format(jsondata))
+	print(type(jsondata))
+	#Return the exported page and attach the filename of the exported WAV file
+	return render_template('exported.html', jsondata=jsondata)
 
 #HELP PAGE
 @website.route('/help')
@@ -162,51 +164,56 @@ def preview_generator():
 		previewFileName = previewFileName.replace('public','..')
 		return jsonify(previewname=previewFileName)
 
-@website.route('/downloader/<action>/<file_format>', methods=['POST'])
-def downloader(action, file_format):
-    if request.method =="POST":
-        if action == "audiofile":
-            #Create WAV file from the string version of noteValues
-            json_data = request.get_json()
-            print(type(json_data))
-            song = Song(None)
-            wavFileName = song.make_wav()
-            wavFileName = wavFileName.replace('wav_outfiles/','')
-            databasename = song.write_to_database()
-            #return redirect('/return-file/{}'.format(filename))
-        elif action == "cowbellfile":
-            return redirect('/return-db/{}'.format(filename))
-    return "You shouldn't be here. GO BACK!"
+@website.route('/downloader', methods=['GET', 'POST'])
+def downloader():
+	if request.method =="POST":
+		if "audioformats" in request.form:
+			if request.form.get("audioformats") == "wav":
+				#Create WAV file from the string version of noteValues
+				# json_data = request.get_json()
+				# print(type(json_data))
+				returnedjson = json.loads(request.form.get("returnedjson"))
+				#"songdata":sNoteValues, "author_name":'Anon', "outfile_name":None, "cloud_db_pos":None}
+				song = Song(None)
+				wavFileName = song.make_wav()
+				wavFileName = wavFileName.replace('wav_outfiles/','')
+				databasename = song.write_to_database()
+				#return redirect('/return-file/{}'.format(filename))
+
+
+		elif action == "cowbellfile":
+			return redirect('/return-db/{}'.format(filename))
+	return "You shouldn't be here. GO BACK!"
 
 @website.route('/get_uid')
 def get_uid():
-    import uuid
-    used_uids = cloud_save.get_uids(SERVER_DB_NAME, SERVER_DB_DIRECTORY)
-    uid = uuid.uuid4().hex
-    while uid in used_uids:
-        uid = uuid.uuid4().hex
-    cloud_save.add_user(SERVER_DB_NAME, SERVER_DB_DIRECTORY, uid)
-    print(uid)
-    return jsonify(uid=uid)
-    
+	import uuid
+	used_uids = cloud_save.get_uids(SERVER_DB_NAME, SERVER_DB_DIRECTORY)
+	uid = uuid.uuid4().hex
+	while uid in used_uids:
+		uid = uuid.uuid4().hex
+	cloud_save.add_user(SERVER_DB_NAME, SERVER_DB_DIRECTORY, uid)
+	print(uid)
+	return jsonify(uid=uid)
+	
 @website.route('/get_project/<uid>/<project_to_open>')
 def get_project(uid, project_to_open):
-    project_data = cloud_save.open_project(SERVER_DB_NAME, SERVER_DB_DIRECTORY, uid, project_to_open)
-    noteDict = {"C4":0, "D4":1, "E4":2, "F4":3, "G4":4, "A4":5, "B4":6, "C5":7}
-    song_read = Song(project_data[1], project_data[2], project_data[4], project_data[0])
-    notes_no = len(song_read.notes_to_play) / 2
-    notes_to_set = [song_read.notes_to_play[i:i+2] for i in range(0, len(song_read.notes_to_play) - 1, 2)]
-    values_to_set = [noteDict[i] for i in notes_to_set]
-    print(values_to_set)
-    return render_template('synth_uploaded.html', notes_no=notes_no, values_to_set=values_to_set)
+	project_data = cloud_save.open_project(SERVER_DB_NAME, SERVER_DB_DIRECTORY, uid, project_to_open)
+	noteDict = {"C4":0, "D4":1, "E4":2, "F4":3, "G4":4, "A4":5, "B4":6, "C5":7}
+	song_read = Song(project_data[1], project_data[2], project_data[4], project_data[0])
+	notes_no = len(song_read.notes_to_play) / 2
+	notes_to_set = [song_read.notes_to_play[i:i+2] for i in range(0, len(song_read.notes_to_play) - 1, 2)]
+	values_to_set = [noteDict[i] for i in notes_to_set]
+	print(values_to_set)
+	return render_template('synth_uploaded.html', notes_no=notes_no, values_to_set=values_to_set)
 
 @website.route('/add_project/<uid>/<project_data>')
 def add_project(uid, project_data):
-    
-    cloud_save.add_project()
-    return None
+	
+	cloud_save.add_project()
+	return None
 
 @website.route('/get_projects/<uid>')
 def get_project_list(uid):
-    projects = cloud_save.list_projects(SERVER_DB_NAME, SERVER_DB_DIRECTORY, uid)
-    return render_template('project_list.html', projects=projects, uid=uid)
+	projects = cloud_save.list_projects(SERVER_DB_NAME, SERVER_DB_DIRECTORY, uid)
+	return render_template('project_list.html', projects=projects, uid=uid)
