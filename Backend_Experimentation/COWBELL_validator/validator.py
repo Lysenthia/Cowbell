96ddd35fa6330b321bd5bb646fef25ef78659fda
Lysenthia <mishtopher@gmail.com>
2017-10-27 09:26:00 +1300

    Working on the validator still

--- Backend_Experimentation/COWBELL_validator/validator.py
+++ Backend_Experimentation/COWBELL_validator/validator.py
@@ -7,11 +7,17 @@ Created on Thu Oct 26 11:04:32 2017
 class CowbellError(Exception):
     """ Invalid Cowbell file """
     
-def validate_cowbell_file(filename):
+def valid_cowbell_file(filename):
     import sqlite3
     db = sqlite3.connect(filename)
     cursor = db.cursor()
-    cursor.execute("""select count(*) from sqlite_master where type='table' and name='song_data'""")
+    try:
+        cursor.execute("""select count(*) from sqlite_master where type='table' and name='song_data'""")
+    except sqlite3.DatabaseError:
+        return False
+    table_check = cursor.fetchone()
+    if not table_check[0]:
+        return False
     db.commit()
     cursor.close()
     db.close()
